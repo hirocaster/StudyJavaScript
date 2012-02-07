4d3be01ea1cee8b2bfed795b86d21430c5c45438
hirocaster <hirocast@gmail.com>
2012-02-07 23:40:32 +0900

    make class

--- BowlingGame/js-test-driver/test/BowlingGameTest.js
+++ BowlingGame/js-test-driver/test/BowlingGameTest.js
@@ -6,12 +6,17 @@ TestCase("BowlingGame", {
   tearDown: function() {
   },
 
-  "test hello": function () {
+  "test hello": function() {
       assertEquals("hello", "hello");
   },
 
-  "test hello2": function () {
-      assertEquals("hello2", "hello2");
+  "test hello2": function() {
+    assertEquals("hello2", "hello2");
+  },
+
+  "test init game class": function() {
+    var game = new BowlingGame();
+    assertEquals("object", typeof(game));
   }
 
 });
