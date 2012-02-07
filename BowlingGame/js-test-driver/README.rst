d3ef51dd25c45b74778f1c90ed38d0b92cdbd3ac
hirocaster <hirocast@gmail.com>
2012-02-07 23:38:12 +0900

    setup js-test-driver

---
+++ BowlingGame/js-test-driver/README.rst
@@ -0,0 +1,14 @@
+==========
+js-test-driverの起動
+==========
+
+  $ java -jar JsTestDriver.jar --port 9876
+
+ブラウザで http://localhost:9876/ にアクセスする。
+ブラウザは立ち上げたままにしておく
+
+==========
+テストの実行
+==========
+
+  $ java -jar JsTestDriver.jar --tests all
---
+++ BowlingGame/js-test-driver/test/BowlingGameTest.js
@@ -0,0 +1,17 @@
+TestCase("BowlingGame", {
+  setUp: function() {
+    console.log("foobaraaa");
+  },
+
+  tearDown: function() {
+  },
+
+  "test hello": function () {
+      assertEquals("hello", "hello");
+  },
+
+  "test hello2": function () {
+      assertEquals("hello2", "hello2");
+  }
+
+});
