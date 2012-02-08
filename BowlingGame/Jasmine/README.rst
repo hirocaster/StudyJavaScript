2c4c7761c9e46bab221408f59c15058f647de280
hirocaster <hirocast@gmail.com>
2012-02-08 23:16:00 +0900

    setup Jasmine

---
+++ BowlingGame/Jasmine/README.rst
@@ -0,0 +1,8 @@
+==========
+Jasmine
+==========
+http://pivotal.github.com/jasmine/
+
+Quick Start
+========
+https://github.com/pivotal/jasmine/wiki/User-Guide
---
+++ BowlingGame/Jasmine/spec/BowlingGameSpec.js
@@ -0,0 +1,13 @@
+describe("BowlingGame", function() {
+
+  var foobar;
+
+  beforeEach(function() {
+    foobar = "foobar";
+  });
+
+  it("Hello Jasmine", function() {
+    expect("Jasmine").toEqual("Jasmine");
+  });
+
+});
